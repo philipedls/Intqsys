dc04d0a69b24356d6e48814a0edf67eb2d1917dd
Philipe <philipe@sparkmobile.com.br>
2021-05-24 10:52:03 -0300

    refactor! array response to json format response on /queue/:date route

--- src/modules/rank/rank.controller.ts
+++ src/modules/rank/rank.controller.ts
@@ -1,9 +1,11 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 import { CraftService } from '../craft/craft.service';
 import { HourlyService } from '../hourly/hourly.service';
 import { PatientsDto } from '../patient/Dto/patients.dto';
 import { PatientService } from '../patient/patient.service';
 import { RankRegisterDto } from './Dto/rank.register.dto';
+import { RankServiceDto } from './Dto/rank.service.dto';
 import { RankService } from './rank.service';
 
 @Controller('queue')
@@ -21,9 +23,9 @@ export class RankController {
     async indexScheduler(@Param() param) {
         console.log(param.date);
         const queues = await this.rankService.findByDate(param.date);
-        return this.patientService.fetchQueueToPatients(queues, this.craftServive, this.hourlyService);
+        const queuedPatients = await this.patientService.fetchQueueToPatients(queues, this.craftServive, this.hourlyService);
 
-        // return { length: queuedPatients.length, list: queuedPatients };
+        return { length: queuedPatients.length, list: queuedPatients };
     }
 
     // @UseGuards(JwtAuthGuard)
