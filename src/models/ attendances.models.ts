72f82b81ea8c89fc0d7659dce01b9b2056995b0e
Philipe Luna <philipe@sparkmobile.com.br>
2021-06-24 23:09:44 -0300

    feat: added attendance id in attendence model

--- src/models/ attendances.models.ts
+++ src/models/ attendances.models.ts
@@ -33,6 +33,12 @@ export class Atttendances {
     @Column('uuid')
     servicos_id_servico: string
 
+    @Column('uuid')
+    atendimentos_id_atendimento: string
+
+    @Column('uuid')
+    pacientes_id_paciente
+
     @OneToOne(() => Services, service => service.id_servico)
     servico: Services
 
